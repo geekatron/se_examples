5e84d08f8b497307072aa82ba3a97d784924a6f1
Adam Nowak <geekatron@gmail.com>
2013-03-19 11:47:56 -0400

    Updating addition logic

--- config/routes/route_resources.js
+++ config/routes/route_resources.js
@@ -34,38 +34,46 @@ module.exports = function(app) {
      * RESTful Examples
      * Addition of Two Numbers
      */
-    app.post('/examples/addition/', function(req, res) {
+     
+     
+    //app.post('/examples/addition/', function(req, res) {
+    app.get('/examples/addition/', function(req, res) {
         //Retrieve the variable from the Query string
         var a = req.query.a,
             b = req.query.b,
-            result = 0;
-
+            result = 0,
+            msg = "";
+            
         //Check to make sure that both variables are defined
+        if( _.isUndefined(a) || _.isUndefined(b) ) {
+            msg = "One of the variables is undefined.";
+            
+            res.send(msg);
+        } else {
+            addNumbers();
+        }
 
-        //Add the two numbers together
-        result = a + b;
-
-        var msg = "The addition of a(" + a + ") and b(" + b + ") is: " + result;
-
-        res.send(msg);
-
-        function handleAuth(err, data) {
-            //Error - handle error response
-            if( !_.isUndefined(err) ) {
-                res.send(err.statuscode, err.message);
+        function addNumbers() {
+            //Check to make sure both are numbers
+            if( isNan(a) || isNan(b) ) {
+                msg = "One or both variables are not a number. Please enter two Integers!";
+                
             } else {
-                //Print out the session id as debug
-                console.log("Session id from report server: " + data );
-
-                var session =
-                {
-                    kind : 'ice-m#SessionId',
-                    sessionid : data
-                };
-
-                res.send(session);
+                //Parse the query string variables as integers
+                a = parseInt(a);
+                b = parseInt(b);
+        
+            
+                //Add the two numbers together
+                result = a + b;
+        
+                msg = "The addition of a(" + a + ") and b(" + b + ") is: " + result;
             }
-        }//END handleAuth
-    });
-
+            
+            //Return the following message
+            res.send(msg);
+        
+        }
+        
+        }); //END GET Addition
 };
